15cad3daaaeaac01769ebf74c31b34963371f1df
Jordi Santiago Provencio <metalzilla@gmail.com>
2015-05-22 21:18:13 +0200

    Little bugfix Server's main

--- src/Biribit/Server/main.cpp
+++ src/Biribit/Server/main.cpp
@@ -77,7 +77,7 @@ int main(int argc, char** argv)
 		if (server.Run(iPort, name.empty() ? nullptr : name.c_str(), pass.empty() ? nullptr : pass.c_str()))
 		{
 			char c;
-			while (server.isRunning() && std::cin >> c)
+			while (server.isRunning())
 				std::this_thread::sleep_for(std::chrono::seconds(1));
 
 			server.Close();
