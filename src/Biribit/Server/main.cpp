762409ce290562769de481475152693004e7a3d5
Jordi Santiago Provencio <metalzilla@gmail.com>
2015-05-22 21:45:54 +0200

    Server daemonized for Linux

--- src/Biribit/Server/main.cpp
+++ src/Biribit/Server/main.cpp
@@ -1,4 +1,5 @@
 #include <Biribit/Server/RakNetServer.h>
+#include <Biribit/Common/PrintLog.h>
 #include <Biribit/BiribitConfig.h>
 #include <Biribit/Common/Types.h>
 
@@ -29,7 +30,76 @@ BOOL WINAPI ConsoleHandlerRoutine(DWORD dwCtrlType)
 #endif
 
 #ifdef SYSTEM_LINUX
+
+// Found here:
+// http://stackoverflow.com/questions/17954432/creating-a-daemon-in-linux
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+
+static void skeleton_daemon()
+{
+	pid_t pid;
+
+	/* Fork off the parent process */
+	pid = fork();
+
+	/* An error occurred */
+	if (pid < 0)
+		exit(EXIT_FAILURE);
+
+	/* Success: Let the parent terminate */
+	if (pid > 0)
+		exit(EXIT_SUCCESS);
+
+	/* On success: The child process becomes session leader */
+	if (setsid() < 0)
+		exit(EXIT_FAILURE);
+
+	/* Catch, ignore and handle signals */
+	//TODO: Implement a working signal handler */
+	signal(SIGCHLD, SIG_IGN);
+	signal(SIGHUP, SIG_IGN);
+
+	/* Fork off for the second time*/
+	pid = fork();
+
+	/* An error occurred */
+	if (pid < 0)
+		exit(EXIT_FAILURE);
+
+	/* Success: Let the parent terminate */
+	if (pid > 0)
+		exit(EXIT_SUCCESS);
+
+	/* Set new file permissions */
+	umask(0);
+
+	/* Change the working directory to the root directory */
+	/* or another appropriated directory */
+	chdir("/");
+
+	/* Close all open file descriptors */
+	int x;
+	for (x = sysconf(_SC_OPEN_MAX); x>0; x--)
+	{
+		close(x);
+	}
+
+	/* Open the log file */
+	openlog("BiribitServer", LOG_PID, LOG_DAEMON);
+}
+
+void STDCALL PrintDaemonLog(const char* msg)
+{
+	syslog(LOG_NOTICE, "First daemon started.");
+}
+
 void TerminationHandler(int signum)
 {
 	server.Close();
@@ -50,7 +120,12 @@ int main(int argc, char** argv)
 		signal(SIGHUP, SIG_IGN);
 	if (signal(SIGTERM, TerminationHandler) == SIG_IGN)
 		signal(SIGTERM, SIG_IGN);
+
+	skeleton_daemon();
+	Log_AddCallback(PrintDaemonLog);
 #endif
+
+	
 
 	try
 	{
@@ -87,6 +162,11 @@ int main(int argc, char** argv)
 	{
 		std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
 	}
+
+#ifdef SYSTEM_LINUX
+	Log_DelCallback(PrintDaemonLog);
+	closelog();
+#endif
 
 	return 0;
 }
